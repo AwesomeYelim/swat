8a8984f6ada8cab4a00d8e93b56ca3b290e7ed6e
Awesomeyelim <uiop01900@gmail.com>
2024-09-19 19:58:18 +0900

    img

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Image from "next/image";
 import { ReactNode } from "react";
 
 export const metadata = {
@@ -12,12 +13,30 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body>
         <div className="container">
           <header>
-            <h1>전국벌레공략도감</h1>
+            <Image
+              // className={extention ? name.split(".")[0] : name}
+              src="/images/logo.svg"
+              alt="logo"
+              loading="eager"
+              priority
+              width={70}
+              height={84}
+            />
+            {/* <h1>전국벌레공략도감</h1> */}
+            <Image
+              // className={extention ? name.split(".")[0] : name}
+              src="/images/main.svg"
+              alt="logo"
+              loading="eager"
+              priority
+              width={348}
+              height={382}
+            />
             <nav>
-              <a href="/about">사업소개</a>
-              <a href="/apply">참가신청</a>
               <a href="/">벌레도감</a>
-              <a href="/">언어&#40;Eng&#41;</a>
+              <a href="/">도시기생충이란</a>
+              <a href="/about">사업소개</a>
+              {/* <a href="/apply">참가신청</a> */}
             </nav>
           </header>
           <main>{children}</main>
